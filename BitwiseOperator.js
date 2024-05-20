5e20b7b5e687daf43b9856d42c29ba69198f9b21
Tam Le <tam.le@dutycast.com>
2024-05-20 22:07:09 +0700

    first commit

---
+++ BitwiseOperator.js
@@ -0,0 +1,13 @@
+if(getFirstNumber() !==0 & getSecondNum() !== 0){
+    console.log('the evalutation was true...');
+}
+
+function getFirstNumber() {
+    console.log('Calling method 1...');
+    return 1;
+}
+
+function getSecondNum() {
+    console.log('Calling method 2...');
+    return 2;
+}
